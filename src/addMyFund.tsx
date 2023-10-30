16a909e0569ad368a9cd7dfa9801881a7751d559
luojinan <luojinan@zhuanzhuan.com>
2023-10-31 00:41:23 +0800

    feat: 获取html并转化为markdown

--- src/addMyFund.tsx
+++ src/addMyFund.tsx
@@ -1,7 +1,25 @@
 import { Detail } from "@raycast/api";
+import { useEffect, useState } from "react";
+import fetch from "node-fetch";
+import { html2Markdown } from '@inkdropapp/html2markdown'
 
 export default function Command() {
-  return (
-    <Detail markdown="hello" />
-  );
+  const [article, setArticle] = useState("");
+  const test = () => {
+    const api = "http://new.xianbao.fun/douban-maizu/2352196.html";
+    return fetch(api).then((item) => {
+      return item.text();
+    });
+  };
+  useEffect(() => {
+    (async () => {
+      const res = await test();
+      const regex = /<article.*?<\/article>/g;
+      const [matches] = res.match(regex) || [];
+      const markdownString = html2Markdown(matches);
+      console.log(markdownString);
+      setArticle(markdownString);
+    })();
+  }, []);
+  return <Detail markdown={article} />;
 }
